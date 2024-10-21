2bda43efc9c8aa1aa0848adf604398983c0c504d
zhangjunjie <ahnu_iot_zhangjunjie@outlook.com>
2024-10-21 20:25:28 +0800

    gitignore

--- mux/lab0/build/obj_dir/board.d
+++
@@ -1,8 +0,0 @@
-board.o: /home/juniezhang/Desktop/nju-digital/mux/lab/csrc/board.cpp \
- Vmux42.h /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- /home/juniezhang/Desktop/ysyx/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/juniezhang/Desktop/ysyx/ysyx-workbench/nvboard/usr/include/pins.h
--- mux/mux21/build/auto_bind.cpp
+++
@@ -1,9 +0,0 @@
-#include <nvboard.h>
-#include "Vmux21.h"
-
-void nvboard_bind_all_pins(Vmux21* top) {
-	nvboard_bind_pin( &top->a, 1, SW7);
-	nvboard_bind_pin( &top->b, 1, SW6);
-	nvboard_bind_pin( &top->s, 1, SW5);
-	nvboard_bind_pin( &top->y, 1, LD15);
-}
